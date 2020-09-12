c14169b8ec5ac7a51990cc7608ff4693bd4c5852
Derevianko Liia <tiwebli@gmail.com>
2020-09-12 12:10:08 +0300

    Все задачи выполнены!

---
+++ js/task-04.js
@@ -0,0 +1,37 @@
+const orderPieces = 7;
+
+const credits = 23580;
+const pricePerDroid = 3000;
+const CANCELED_BY_USER = 'Отменено пользователем!';
+const ACCESS_DENIED = 'Недостаточно средств на счету!';
+
+let totalPrice; // Write code on this line
+totalPrice = pricePerDroid * orderPieces;
+let balanceCredit; // Write code on this line
+balanceCredit = credits - totalPrice;
+let message;
+
+// Write code under this line
+
+if (orderPieces <= 7 && orderPieces < 12) {
+  message = `Вы купили ${orderPieces} дроидов, на счету осталось ${balanceCredit} кредитов`;
+} else {
+  message = ACCESS_DENIED;
+}
+if (orderPieces === null) {
+  message = CANCELED_BY_USER;
+}
+
+console.log(message);
+
+//если orderPieces равно 4
+// то значение message будет равно
+// 'Вы купили 4 дроидов, на счету осталось 11580 кредитов'
+
+//если orderPieces равно null
+// то значение message будет равно
+// 'Отменено пользователем!'
+
+//если orderPieces равно 10
+// то значение message будет равно
+// 'Недостаточно средств на счету!'
